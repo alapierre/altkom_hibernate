11d790939c758c3bb69511850b59aa2d9ebfe585
Rafal <rafik991@gmail.com>
2014-10-22 12:23:25 +0200

    User

---
+++ moto-crm-model/src/main/java/pl/altkom/moto/crm/model/User.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.altkom.moto.crm.model;
+
+/**
+ *
+ * @author Administrator
+ */
+public class User extends BaseEntity {
+
+    private String login;
+    private String password;
+    private boolean active;
+    private Group group;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    }
+
+}

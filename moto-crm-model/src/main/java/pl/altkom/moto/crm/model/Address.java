56f1aab7db082796f571350a7eb425bc346c8345
Administrator <Administrator@192.168.1.99>
2014-10-22 12:24:18 +0200

    Added Address

---
+++ moto-crm-model/src/main/java/pl/altkom/moto/crm/model/Address.java
@@ -0,0 +1,35 @@
+package pl.altkom.moto.crm.model;
+
+public class Address extends BaseEntity {
+
+    private String city;
+
+    private String street;
+
+    private String postCode;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+}

f60eae430c72ef604d62a387ddfddade6008fa93
Grzegorz Swit <gswit@mail>
2014-10-22 14:38:13 +0200

    marked Address as entity

--- moto-crm-model/src/main/java/pl/altkom/moto/crm/model/Address.java
+++ moto-crm-model/src/main/java/pl/altkom/moto/crm/model/Address.java
@@ -1,11 +1,21 @@
 package pl.altkom.moto.crm.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@Entity
 public class Address extends BaseEntity {
 
+    @NotNull
     private String city;
 
+    @NotNull
     private String street;
 
+    @Column(name = "POST_CODE", nullable = false)
+    @Pattern(regexp = "^\\d{2}-\\d{3}$")
     private String postCode;
 
     public String getCity() {

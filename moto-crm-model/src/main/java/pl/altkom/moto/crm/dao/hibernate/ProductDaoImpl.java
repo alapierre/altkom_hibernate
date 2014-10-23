ffef3162714916fd30f2cb2af2f261bf7c476775
jaceks <jaceks@com.com>
2014-10-23 12:08:26 +0200

    Product dao added

---
+++ moto-crm-model/src/main/java/pl/altkom/moto/crm/dao/hibernate/ProductDaoImpl.java
@@ -0,0 +1,21 @@
+package pl.altkom.moto.crm.dao.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import pl.altkom.moto.crm.model.Product;
+
+/**
+ *
+ * @author Administrator
+ */
+@Repository
+public class ProductDaoImpl implements ProductDao {
+    @Autowired
+    private SessionFactory sessionFactory;
+    
+    @Override
+    public Product find(Long id) {
+        return (Product) sessionFactory.getCurrentSession().get(Product.class, id);
+    }
+}
